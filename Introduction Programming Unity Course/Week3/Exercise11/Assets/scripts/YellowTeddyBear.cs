a01f1ca39ea1ee75091defc01d1f563f33d6ede0
Eduardo Santos <ejmorbey@hotmail.com>
2018-01-16 00:28:56 +0000

    Exercise11
    Use transform to set Sprites Scale

---
+++ Introduction Programming Unity Course/Week3/Exercise11/Assets/scripts/YellowTeddyBear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowTeddyBear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//  quadruple  width  and  height
+		Vector3 newVector = transform.localScale;
+		newVector.x *= 4;
+		newVector.y *= 4;
+
+		transform.localScale = newVector;
+	}
+
+}
