92c52053ea3caa575189319e5edca29dc191b61a
Eduardo Santos <ejmorbey@hotmail.com>
2018-01-16 10:44:11 +0000

    Exercise13
    Using prefabs

---
+++ Introduction Programming Unity Course/Week 3/Exercise13/Assets/scripts/TeddyBear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeddyBear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().AddForce(
+			new Vector2(3,0),
+			ForceMode2D.Impulse);
+	}
+
+	/// <summary>
+	/// Perform actions on collision detection
+	/// </summary>
+	/// <param name="coll">Collision</param>
+	void OnCollisionEnter2D(Collision2D coll) {
+		print ("Ouch");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
