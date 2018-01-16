424457cf30022c5d90ed6e6f5eaaa6e96f3e2618
Eduardo Santos <ejmorbey@hotmail.com>
2018-01-16 09:12:55 +0000

    Exercise12
    Apply impulse force to a sprite

---
+++ Introduction Programming Unity Course/Week 3/Exercise12/Assets/scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves the object
+/// </summary>
+public class Mover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Get the game object moving
+		GetComponent<Rigidbody2D>().AddForce(
+			new Vector2(0 ,5),
+			ForceMode2D.Impulse);
+	}
+}
