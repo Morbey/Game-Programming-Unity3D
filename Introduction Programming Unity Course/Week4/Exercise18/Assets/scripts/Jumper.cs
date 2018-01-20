bf4b256f9e0f539c87fd48594a480545ad69ad0d
Eduardo Santos <ejmorbey@hotmail.com>
2018-01-20 17:10:34 +0000

    Exercise18
    Input Mouse location and mouse click

---
+++ Introduction Programming Unity Course/Week4/Exercise18/Assets/scripts/Jumper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Jumps the game object to the mouse location when the left mouse button is pressed.
+/// </summary>
+public class Jumper : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		// If the left mouse button is clicked do stuff
+		if (Input.GetMouseButtonDown (0)) {
+			// Get the mouse position
+			Vector3 position = Input.mousePosition;
+			position.z = -Camera.main.transform.position.z;
+			position = Camera.main.ScreenToWorldPoint(position);
+
+			// Move the game object to the mouse position
+
+			transform.position = position;
+		}
+	}
+}
