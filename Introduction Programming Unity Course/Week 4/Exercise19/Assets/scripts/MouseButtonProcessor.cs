a2c090b26059325d96e84507bcda986e6928e251
Eduardo Santos <ejmorbey@hotmail.com>
2018-01-20 21:16:05 +0000

    Exercise18
    Spawn and Explode

---
+++ Introduction Programming Unity Course/Week 4/Exercise19/Assets/scripts/MouseButtonProcessor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Processes mouse button inputs
+/// </summary>
+public class MouseButtonProcessor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabExplosion;
+    [SerializeField]
+    public GameObject prefabTeddyBear;
+
+    // first frame input support
+    bool spawnInputOnPreviousFrame = false;
+	bool explodeInputOnPreviousFrame = false;
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+
+        // spawn teddy bear as appropriate
+		if (Input.GetAxis ("SpawnTeddyBear") > 0) {
+			if (!spawnInputOnPreviousFrame) {
+				// getMouseLocation
+				Vector3 location = Input.mousePosition;
+				location.z = -Camera.main.transform.position.z;
+				location = Camera.main.ScreenToWorldPoint (location);
+
+				spawnInputOnPreviousFrame = true;
+				Instantiate<GameObject> (prefabTeddyBear, location, Quaternion.identity);
+			}
+		}
+		else {
+			spawnInputOnPreviousFrame = false;
+		}
+
+		// explode teddy bear as appropriate
+		if (Input.GetAxis ("ExplodeTeddyBear") > 0) {
+			if (!explodeInputOnPreviousFrame) {
+				// getOneTeddyBearToExplode
+				GameObject teddy = GameObject.FindGameObjectWithTag("TeddyBear");
+				if (teddy != null) {
+					Destroy (teddy);
+					Instantiate<GameObject> (prefabExplosion, teddy.transform.position, Quaternion.identity);
+				}
+				explodeInputOnPreviousFrame = true;
+			}
+		}
+		else {
+			explodeInputOnPreviousFrame = false;
+		}		
+	}
+}
