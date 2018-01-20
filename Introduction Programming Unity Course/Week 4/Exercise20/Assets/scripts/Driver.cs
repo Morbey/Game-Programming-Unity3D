19fbb21a094dc628c2b2c5749d09f2641689216a
Eduardo Santos <ejmorbey@hotmail.com>
2018-01-20 21:30:51 +0000

    Exercise20
    Driving a gameObject

---
+++ Introduction Programming Unity Course/Week 4/Exercise20/Assets/scripts/Driver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Drives the game object based on keyboard input
+/// </summary>
+public class Driver : MonoBehaviour {
+	
+	//store how many units your game object moves per second 
+	const float MoveUnitsPerSecond = 5;
+
+	// Save the value on the Horizontal input axis
+	void Update () {
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		float verticalInput = Input.GetAxis ("Vertical");
+		Vector3 position = transform.position;
+
+		if (horizontalInput != 0) {
+			position.x += horizontalInput * MoveUnitsPerSecond * Time.deltaTime;
+		}
+		if (verticalInput != 0) {
+			position.y += verticalInput * MoveUnitsPerSecond * Time.deltaTime;
+		}
+
+		transform.position = position;
+	}
+}
